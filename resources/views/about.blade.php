4434dad837b9bdda9abe4cccca7e7457aead252d
Katarzyna Kuc <kuckatarzyna399@gmail.com>
2022-02-01 10:47:02 +0100

    initial commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title></title>
+        <!-- <link rel="stylesheet" type="text/css" href="/css/app.css"> -->
+    </head>
+    <body>
+        <h1>About me</h1>
+
+        <p>
+            Jakiś bardzo długi tekst.
+        </p>
+    </body>
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title></title>
+    </head>
+    <body>
+        <h1>First App</h1>
+
+        <p>
+            <a href="/contact">Contact</a> for more informations.
+        </p>
+    </body>
+</html>
